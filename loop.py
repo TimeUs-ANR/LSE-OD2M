f1053d90b347f3d9712d40115f9f47cfa41ffd2f
Alix Chagué <alix.chague@inria.fr>
2018-11-09 12:09:18 +0100

    added small tool to treat several documents

---
+++ loop.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+document = os.listdir(os.path.join(CWD, "input"))
+clear_doc = []
+for doc in document:
+    name, ext = doc.split(".")
+    if ext == "xml":
+        clear_doc.append("input/" + name + "." + ext)
+for d in clear_doc:
+    subprocess.call(["python3", "xml2txt.py", "-i", d])
