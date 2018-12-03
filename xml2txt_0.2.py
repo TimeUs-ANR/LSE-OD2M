5dff1f1b433378e9da3988605ad030b49985ea8d
Alix Chagué <alix.chague@chartes.psl.eu>
2018-12-03 13:57:15 +0100

    continue adding modularity

--- xml2txt_0.2.py
+++ xml2txt_0.2.py
@@ -70,6 +70,134 @@ def write_output(filename, content):
         f.write(content)
 
 
+def rearrange(soup):
+    """
+
+    :param soup:
+    :return:
+    """
+    all_pages = soup.find_all("page")
+    for page in all_pages:
+        all_blocks = page.find_all("block")
+        for block in all_blocks:
+            # modify figure type blocks, including tables
+            if block["blocktype"] != "Text":
+                block.name = "figure"
+                block["type"] = block["blocktype"]
+                attrs_list = block.attrs
+                for attr in list(attrs_list):
+                    if attr != "type":
+                        del block[attr]
+                block.clear()
+            # rearrange text type blocks
+            else:
+                if block.region:
+                    block.region.decompose()
+                # moving par elements right under block element
+                all_pars = block.find_all("par")
+                for par in all_pars:
+                    ext_par = par.extract()
+                    ext_par.name = "p"
+                    block.append(ext_par)
+                # finding a way around to delete tag names text
+                all_tags = block.contents
+                for tag in all_tags:
+                    if tag.name == "text":
+                        tag.decompose()
+                # moving line elements right under par element
+                all_lines = block.find_all("line")
+                for line in all_lines:
+                    line.append(line.formatting.string)
+                    f_attrs_list = line.formatting.attrs
+                    if len(f_attrs_list) > 0:
+                        for f_attr in f_attrs_list:
+                            line[f_attr] = f_attrs_list[f_attr]
+                    line.formatting.decompose()
+                block["type"] = "Text"
+                del block["blockname"]
+                block.name = "div"
+    return soup
+
+
+def exclude_headers(soup):
+    """
+    
+    :param soup: 
+    :return: 
+    """
+    guard = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?><document xmlns="http://www.abbyy.com/FineReader_xml/FineReader10-schema-v1.xml" version="1.0" producer="timeUs"></document>"""
+    guard_soup = BeautifulSoup(guard, "lxml-xml")
+
+    all_pages = soup.find_all("page")
+    warning_headers = []
+    warning_signatures = []
+    count_page = 0
+    for page in all_pages:
+        count_page += 1
+        page["id"] = "page%s" % count_page
+        page_f = copy(page)
+        page_f.clear()
+        all_divs = page.find_all("div")
+        count_div = 0
+        for div in all_divs:
+            count_div += 1
+            div["id"] = "page%s_div%s" % (count_page, count_div)
+            div_f = copy(div)
+            div_f.clear()
+            all_ps = div.find_all("p")
+            count_p = 0
+            for p in all_ps:
+                count_p += 1
+                p["id"] = "page%s_div%s_p%s" % (count_page, count_div, count_p)
+                p_f = copy(p)
+                p_f.clear()
+                all_lines = p.find_all("line")
+                count_line = 0
+                for line in all_lines:
+                    id_line = "page%s_d%s_p%s_l" % (count_page, count_div, count_p)
+                    # targetting headers
+                    if int(line["b"]) < (int(page["height"]) * 0.12):
+                        if "linespacing" in line.parent.attrs:
+                            if (int(line.parent["linespacing"]) <= 750) and (int(line.parent["linespacing"]) >= 390):
+                                line_f = line.extract()
+                                line_f["type"] = "header"
+                                p_f.append(line_f)
+                            else:
+                                count_line += 1
+                                line["id"] = id_line + str(count_line)
+                                test_str = line.string
+                                if len(test_str.replace(" ", "")) < 55:
+                                    warning_headers.append((line["id"], line.string))
+                        else:
+                            count_line += 1
+                            line["id"] = id_line + str(count_line)
+                            test_str = line.string
+                            if len(test_str.replace(" ", "")) < 55:
+                                warning_headers.append((line["id"], line.string))
+                    # targetting signatures
+                    elif int(line["b"]) > (int(page["height"]) * 0.91):
+                        if len(line.string) <= 2:
+                            line_f = line.extract()
+                            line_f["type"] = "signature"
+                            p_f.append(line_f)
+                        elif len(line.string) >= 3 and len(line.string) < 10:
+                            count_line += 1
+                            line["id"] = id_line + str(count_line)
+                            warning_signatures.append((line["id"], line.string))
+                        else:
+                            count_line += 1
+                            line["id"] = id_line + str(count_line)
+                    else:
+                        count_line += 1
+                        line["id"] = id_line + str(count_line)
+                if len(p_f.contents) > 0:
+                    div_f.append(p_f)
+            if len(div_f.contents) > 0:
+                page_f.append(div_f)
+        guard_soup.document.append(page_f)
+    return guard_soup, soup, warning_headers, warning_signatures
+
+
 def make_text(input, output=False):
     """ Perform transformation to raw text, adding markers
 
@@ -79,118 +207,10 @@ def make_text(input, output=False):
     :type output: list if True
     :return:
     """
-    soup = make_the_soup(input)
-    if soup:
-        all_pages = soup.find_all("page")
-        for page in all_pages:
-            all_blocks = page.find_all("block")
-            for block in all_blocks:
-                # modify figure type blocks, including tables
-                if block["blocktype"] != "Text":
-                    block.name = "figure"
-                    block["type"] = block["blocktype"]
-                    attrs_list = block.attrs
-                    for attr in list(attrs_list):
-                        if attr != "type":
-                            del block[attr]
-                    block.clear()
-                # rearrange text type blocks
-                else:
-                    if block.region:
-                        block.region.decompose()
-                    # moving par elements right under block element
-                    all_pars = block.find_all("par")
-                    for par in all_pars:
-                        ext_par = par.extract()
-                        ext_par.name = "p"
-                        block.append(ext_par)
-                    # finding a way around to delete tag names text
-                    all_tags = block.contents
-                    for tag in all_tags:
-                        if tag.name == "text":
-                            tag.decompose()
-                    # moving line elements right under par element
-                    all_lines = block.find_all("line")
-                    for line in all_lines:
-                        line.append(line.formatting.string)
-                        f_attrs_list = line.formatting.attrs
-                        if len(f_attrs_list) > 0:
-                           for f_attr in f_attrs_list:
-                               line[f_attr] = f_attrs_list[f_attr]
-                        line.formatting.decompose()
-                    block["type"] = "Text"
-                    del block["blockname"]
-                    block.name = "div"
-        # preparing alternative soup with only removed items
-        guard = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?><document xmlns="http://www.abbyy.com/FineReader_xml/FineReader10-schema-v1.xml" version="1.0" producer="timeUs"></document>"""
-        guard_soup = BeautifulSoup(guard, "lxml-xml")
-        all_pages = soup.find_all("page")
-        warning_headers = []
-        warning_signatures = []
-        count_page = 0
-        for page in all_pages:
-            count_page += 1
-            page["id"] = "page%s" % count_page
-            page_f = copy(page)
-            page_f.clear()
-            all_divs = page.find_all("div")
-            count_div = 0
-            for div in all_divs:
-                count_div += 1
-                div["id"] = "page%s_div%s" % (count_page, count_div)
-                div_f = copy(div)
-                div_f.clear()
-                all_ps = div.find_all("p")
-                count_p = 0
-                for p in all_ps:
-                    count_p += 1
-                    p["id"] = "page%s_div%s_p%s" % (count_page, count_div, count_p)
-                    p_f = copy(p)
-                    p_f.clear()
-                    all_lines = p.find_all("line")
-                    count_line = 0
-                    for line in all_lines:
-                        id_line = "page%s_d%s_p%s_l" % (count_page, count_div, count_p)
-                        # targetting headers
-                        if int(line["b"]) < (int(page["height"]) * 0.12):
-                            if "linespacing" in line.parent.attrs:
-                                if (int(line.parent["linespacing"]) <= 750) and (int(line.parent["linespacing"]) >= 390):
-                                    line_f = line.extract()
-                                    line_f["type"] = "header"
-                                    p_f.append(line_f)
-                                else:
-                                    count_line += 1
-                                    line["id"] = id_line + str(count_line)
-                                    test_str = line.string
-                                    if len(test_str.replace(" ", "")) < 55:
-                                        warning_headers.append((line["id"], line.string))
-                            else:
-                                count_line += 1
-                                line["id"] = id_line + str(count_line)
-                                test_str = line.string
-                                if len(test_str.replace(" ", "")) < 55:
-                                    warning_headers.append((line["id"], line.string))
-                        # targetting signatures
-                        elif int(line["b"]) > (int(page["height"]) * 0.91):
-                            if len(line.string) <= 2:
-                                line_f = line.extract()
-                                line_f["type"] = "signature"
-                                p_f.append(line_f)
-                            elif len(line.string) >= 3 and len(line.string) < 10:
-                                count_line += 1
-                                line["id"] = id_line + str(count_line)
-                                warning_signatures.append((line["id"], line.string))
-                            else:
-                                count_line += 1
-                                line["id"] = id_line + str(count_line)
-                        else:
-                            count_line += 1
-                            line["id"] = id_line + str(count_line)
-                    if len(p_f.contents) > 0:
-                        div_f.append(p_f)
-                if len(div_f.contents) > 0:
-                    page_f.append(div_f)
-            guard_soup.document.append(page_f)
+    transformed_text = make_the_soup(input)
+    if transformed_text:
+        transformed_text = rearrange(transformed_text)
+        transformed_text_guard, transformed_text, warning_headers, warning_signatures = exclude_headers(transformed_text)
 
         # ...
         # - recompose paragraphs
@@ -198,13 +218,15 @@ def make_text(input, output=False):
         # - add management of location within the article from titles and headers
 
         report_warnings(warning_headers, warning_signatures)
-        final_out = BeautifulSoup("", "lxml-xml")
-        final_out.append(soup.document)
-        final_str = str(final_out.prettify())
-        guard_str = str(guard_soup.prettify())
+
+        final_text = BeautifulSoup("", "lxml-xml")
+        final_text.append(transformed_text.document)
+        final_text_str = str(final_text.prettify())
+        guard_str = str(transformed_text_guard.prettify())
 
         out_file, output_guard = make_out_filenames(input, output)
-        write_output(out_file, final_str)
+
+        write_output(out_file, final_text_str)
         write_output(output_guard, guard_str)
     return
 
