c5909abc01069067e58e9bc8ff5761b16d7ee153
Alix Chagué <alix.chague@inria.fr>
2018-12-13 10:51:08 +0100

    final touch to utils.utils.report()

--- structure_extraction/utils/utils.py
+++ structure_extraction/utils/utils.py
@@ -15,32 +15,19 @@ def is_number(s):
         return False
 
 
-def report_warnings(warning_headers, warning_signatures):
-    """Print warnings in terminal
-
-    :param warning_headers: list of potentially missed headers
-    :param warning_signatures: list of potentially missed signatures
-    """
-    for warn_id, warn_string in warning_headers:
-        print(colored("WARNING:", "yellow", attrs=["reverse"]),
-              "Might be a HEADER but was left in output : '{}':\n\t".format(warn_id),
-              colored(warn_string, "white", attrs=["dark"]))
-    for warn_id, warn_string in warning_signatures:
-        print(colored("WARNING:", "yellow", attrs=["reverse"]),
-              "Might be a SIGNATURE but was left in output : '{}':\n\t".format(warn_id),
-              colored(warn_string, "white", attrs=["dark"]))
-
 def report(warn_list, topic):
-    """
+    """Use logging to print messages in the terminal with different formats depending on the topic.
 
-    :param warn_list:
-    :param topic:
+    :param warn_list: list of warning messages
+    :type warn_list: list
+    :param topic: topic of the warnings
+    :type topic: string
     :return:
     """
-    logging.basicConfig(format='%(levelname))s:%(message)s')
+    logging.basicConfig(format='%(levelname)s:%(message)s')
     if topic == "SIGNATURE":
         for warn_id, warn_string in warn_list:
-            message = "Might be a {} but was left in the output: '{}':\n{}".format(
+            message = "Might be a {} but was left in the output: '{}':\n\t{}".format(
                 colored(topic, "yellow"),
                 colored(warn_id, "yellow"),
                 colored(warn_string, "white", attrs=["dark"])
@@ -49,19 +36,19 @@ def report(warn_list, topic):
 
     elif topic == "HEADER":
         for warn_id, warn_string, corr in warn_list:
-            message = "Might be a {} but was left in the output: '{}':\n{}\nis it: {}".format(
+            message = "Might be a {} but was left in the output: '{}':\n\t{}\n\tis it the header: '{}'?".format(
                 colored(topic, "yellow"),
                 colored(warn_id, "yellow"),
                 colored(warn_string, "white", attrs=["dark"]),
-                colored(corr, "blue", attrs=["dark"])
+                colored(corr, "white", attrs=["dark"])
             )
             logging.warning(message)
 
     elif topic == "CORRECT_HEADER":
         for warn_id, head_orig, head_corr in warn_list:
-            message = "You may want to verify this correction I made in '{}':\nfrom: {}\nto  : {}".format(
+            message = "You may want to verify this correction I made in '{}':\n\tfrom: {}\n\tto  : {}".format(
                 colored(warn_id, "blue"),
-                head_orig,
+                colored(head_orig, "white", attrs=["dark"]),
                 colored(head_corr, "white", attrs=["dark"])
             )
             logging.warning(message)
